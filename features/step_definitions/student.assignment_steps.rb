eb9af458cb687fbbcc815314a97af2e5e8cd6e96
Em01 <emma_williams1@hotmail.co.uk>
2015-01-23 22:44:20 +0000

    all initial tests pass and my teacher should have my assignments is initially setup with teacher class

--- features/step_definitions/student.assignment_steps.rb
+++ features/step_definitions/student.assignment_steps.rb
@@ -1,6 +1,7 @@
 
 Given(/^I am a student$/) do
   @student = Student.new #setting as an instance variable to access later on
+  @teacher = Teacher.new
 end
 
 When(/^I submit an assignment to my teacher$/) do
@@ -9,5 +10,5 @@ When(/^I submit an assignment to my teacher$/) do
 end
 
 Then(/^my teacher should have my assignment$/) do
-  pending # express the regexp above with the code you wish you had
+	expect(@teacher.assignment_for_student(@student)).to eq(@assingment)
 end
---
+++ lib/teacher.rb
@@ -0,0 +1,4 @@
+class Teacher 
+	def assignment_for_student(student)
+	end
+end
