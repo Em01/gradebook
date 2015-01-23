5a98c205cf136046ddbd4472cba437314366f723
Em01 <emma_williams1@hotmail.co.uk>
2015-01-23 18:11:29 +0000

    initial setup for student step definitions

---
+++ features/step_definitions/student.assignment_steps.rb
@@ -0,0 +1,12 @@
+
+Given(/^I am a student$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+When(/^I submit an assignment to my teacher$/) do
+  pending # express the regexp above with the code you wish you had
+end
+
+Then(/^my teacher should have my assignment$/) do
+  pending # express the regexp above with the code you wish you had
+end
