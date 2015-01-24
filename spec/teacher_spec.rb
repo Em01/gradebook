16d4ed6da85ac6b4ea93700631626efb6489a62c
Em01 <emma_williams1@hotmail.co.uk>
2015-01-24 09:49:11 +0000

    amended stubs to doubles to keep with correct rspec syntax

--- spec/teacher_spec.rb
+++ spec/teacher_spec.rb
@@ -2,5 +2,10 @@ require_relative "../lib/teacher"
 require "rspec"
 
 describe Teacher do 
-	it "should stoe assignments"
+	it "should store assignments" do
+		student = double
+		assignment = double
+		subject.submit_assignment(student, assignment)
+		expect(subject.assignment_for_student(student)).to eq(assignment)
+	end
 end
